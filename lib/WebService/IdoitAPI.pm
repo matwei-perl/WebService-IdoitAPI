a7670255bf6110d1dc30a6a1478ea7d38945c2d4
Mathias Weidner <mamawe@cpan.org>
2022-10-03 17:48:33 +0200

    create content with module-starter

---
+++ lib/WebService/IdoitAPI.pm
@@ -0,0 +1,105 @@
+package WebService::IdoitAPI;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+WebService::IdoitAPI - The great new WebService::IdoitAPI!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use WebService::IdoitAPI;
+
+    my $foo = WebService::IdoitAPI->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Mathias Weidner, C<< <mamawe at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-webservice-idoitapi at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=WebService-IdoitAPI>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc WebService::IdoitAPI
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=WebService-IdoitAPI>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/WebService-IdoitAPI>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/WebService-IdoitAPI>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2022 by Mathias Weidner.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of WebService::IdoitAPI
